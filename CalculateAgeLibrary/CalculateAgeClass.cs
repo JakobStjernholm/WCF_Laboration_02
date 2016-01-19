50340e03011465b5f3607ae9fad76ac24393865a
Jakob Stjernholm <jakob.stjerna@gmail.com>
2016-01-19 14:04:53 +0100

    Task_02 done

---
+++ CalculateAgeLibrary/CalculateAgeClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateAgeLibrary
+{
+    public class CalculateAgeClass : ICalculateAgeClass
+    {
+        public int GetDays(DateTime birthday)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthday.Year;
+            return age;
+        }
+    }
+    [ServiceContract(Namespace = "http://CalculateAgeLibrary")]
+    public interface ICalculateAgeClass
+    {
+        [OperationContract]
+        int GetDays(DateTime birthday);
+    }
+}
---
+++ CalculateAgeProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using CalculateAgeLibrary;
+
+namespace CalculateAgeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/CalculateAgeLibrary");
+            ServiceHost host = new ServiceHost(typeof(CalculateAgeClass),baseAddress);
+            //using (host)
+            //{
+                host.AddServiceEndpoint(typeof(ICalculateAgeClass),
+                    new WSHttpBinding(),
+                    "CalculateAgeClass");
+
+                ServiceMetadataBehavior serviceMetadataBehavior = new ServiceMetadataBehavior();
+                serviceMetadataBehavior.HttpGetEnabled = true;
+                host.Description.Behaviors.Add(serviceMetadataBehavior);
+
+                host.Open();
+                Console.WriteLine("Tjänsten startas!! :)");
+                Console.ReadLine();
+            host.Close();
+            //}
+
+        }
+    }
+}
